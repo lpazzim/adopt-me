e5b257ca30cac763c3c93065c2956014fc669bbc
Lucas Pazzim <lpazzim@gmail.com>
2023-08-16 12:22:58 -0300

    useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Adopt Me!</h1>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </div>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
     </BrowserRouter>
   );
 };

063a35a68493362415cf459714ac58b4ec1b8232
Lucas Pazzim <lpazzim@gmail.com>
2023-08-18 11:27:59 -0300

    Handling Events in Class Components part 1

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,6 +9,12 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -18,8 +24,11 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
+              onClick={this.handleIndexClick}
               key={photo}
+              data-index={index}
               src={photo}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
